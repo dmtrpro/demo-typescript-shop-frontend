e9152ec3776e718a40b9853f16d0f64eb51e5203
DmtrPro <dmtr.pros@gmail.com>
2018-01-16 00:33:08 +0300

    categories

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -523,6 +523,9 @@ $(function () {
     cartCounter.hideOnNull = true;
     cartCounter.attach('#cart-counter', true);
     cartTotal.attach('#cart-total', true);
+    $('.categories').on('click', '.categories-single', function (e) {
+        $(this).toggleClass('active');
+    });
 });
 
 
